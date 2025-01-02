39d41d91955af793a963326ca5ac3bba0efb9de3
Chellil14 <chellilvargas14@gmail.com>
2025-01-02 06:51:03 -0600

    Initialize sckp-chrome-upstream

---
+++ shell/browser/electron_pdf_document_helper_client.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Slack Technologies, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "shell/browser/electron_pdf_document_helper_client.h"
+
+#include "content/public/browser/web_contents.h"
+
+ElectronPDFDocumentHelperClient::ElectronPDFDocumentHelperClient() = default;
+ElectronPDFDocumentHelperClient::~ElectronPDFDocumentHelperClient() = default;
