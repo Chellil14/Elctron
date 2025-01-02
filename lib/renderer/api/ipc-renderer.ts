c4b6a476e048d13e6552e7d7fb064d5e1d688d2b
Chellil14 <chellilvargas14@gmail.com>
2025-01-02 06:38:30 -0600

    Initialize 27-x-y

---
+++ lib/renderer/api/ipc-renderer.ts
@@ -0,0 +1,39 @@
+import { EventEmitter } from 'events';
+import * as deprecate from '@electron/internal/common/deprecate';
+
+const { ipc } = process._linkedBinding('electron_renderer_ipc');
+
+const internal = false;
+
+const ipcRenderer = new EventEmitter() as Electron.IpcRenderer;
+ipcRenderer.send = function (channel, ...args) {
+  return ipc.send(internal, channel, args);
+};
+
+ipcRenderer.sendSync = function (channel, ...args) {
+  return ipc.sendSync(internal, channel, args);
+};
+
+ipcRenderer.sendToHost = function (channel, ...args) {
+  return ipc.sendToHost(channel, args);
+};
+
+const sendToDeprecated = deprecate.warnOnce('ipcRenderer.sendTo');
+ipcRenderer.sendTo = function (webContentsId, channel, ...args) {
+  sendToDeprecated();
+  return ipc.sendTo(webContentsId, channel, args);
+};
+
+ipcRenderer.invoke = async function (channel, ...args) {
+  const { error, result } = await ipc.invoke(internal, channel, args);
+  if (error) {
+    throw new Error(`Error invoking remote method '${channel}': ${error}`);
+  }
+  return result;
+};
+
+ipcRenderer.postMessage = function (channel: string, message: any, transferables: any) {
+  return ipc.postMessage(channel, message, transferables);
+};
+
+export default ipcRenderer;
