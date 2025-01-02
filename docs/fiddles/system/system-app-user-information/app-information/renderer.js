271a9b40ae9e1659992ac75bc6c60d12e354ae77
Chellil14 <chellilvargas14@gmail.com>
2025-01-02 06:26:54 -0600

    Initialize fix-visibilitystate-webview

---
+++ docs/fiddles/system/system-app-user-information/app-information/renderer.js
@@ -0,0 +1,9 @@
+const { ipcRenderer } = require('electron')
+
+const appInfoBtn = document.getElementById('app-info')
+
+appInfoBtn.addEventListener('click', async () => {
+  const path = await ipcRenderer.invoke('get-app-path')
+  const message = `This app is located at: ${path}`
+  document.getElementById('got-app-info').innerHTML = message
+})
