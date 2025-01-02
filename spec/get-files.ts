3513e432320a94bcadcf9d6bdee3bc7b3de0e5c8
Chellil14 <chellilvargas14@gmail.com>
2025-01-02 07:00:34 -0600

    Initialize trop/31-x-y-bp-build-remove-unused-lint-package-1730573850671

---
+++ spec/get-files.ts
@@ -0,0 +1,15 @@
+import * as walkdir from 'walkdir';
+
+import { once } from 'node:events';
+
+export async function getFiles (directoryPath: string, { filter = null }: {filter?: ((file: string) => boolean) | null} = {}) {
+  const files: string[] = [];
+  const walker = walkdir(directoryPath, {
+    no_recurse: true
+  });
+  walker.on('file', (file) => {
+    if (!filter || filter(file)) { files.push(file); }
+  });
+  await once(walker, 'end');
+  return files;
+}
